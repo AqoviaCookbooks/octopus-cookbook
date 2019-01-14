a26fd2dbe86a5a6d6bacebb3287d09b344bb5ca2
Onat Erses <onat.erses@aqovia.com>
2019-01-14 17:55:00 +0000

    File download test

--- recipes/install_tentacle.rb
+++ recipes/install_tentacle.rb
@@ -45,6 +45,6 @@ end
   # not_if { ::File.exists?(octo_exe_path) } 
 # end
 
-file win_friendly_path("#{tools['home']}/OctopusTools.4.17.0.zip") do
+cookbook_file win_friendly_path("#{tools['home']}/OctopusTools.4.17.0.zip") do
   source tools['url']
 end
